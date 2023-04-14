98f03609e4ebbbbf08c6f4fd24d617a69c34fdc4
jim raynor <fallere82531@gmail.com>
2023-04-14 21:03:26 +0900

    Docs: Find out the properties of the class in order to find out the properties of the class, we cloned the class.

--- HaaardWork/Program.cs
+++ HaaardWork/Program.cs
@@ -2,12 +2,25 @@
 {
     internal class Program
     {
+        class MyClass
+        {
+            public int a;
+            public int b;
+        }
         static void Main(string[] args)
         {
-            //다음 코드에서 a는 어떤 값을 가지는가?
-            int a = 0xF0 | 0x0F;
-            Console.WriteLine(a);
-            //64
+            //다음과 같은 코드가 있다고 가정할 때 출력결과와 그 이유를 서술하라
+            MyClass source = new MyClass();
+            source.a = 10;
+            source.b = 20;
+
+            MyClass target = source;
+            target.b = 100;
+
+            Console.WriteLine("{0}, {1}", source.a, source.b); 
+            Console.WriteLine("{0}, {1}", target.a, target.b);
+            //10 100
+            //10 100
         }
     }
 }
