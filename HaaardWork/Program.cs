c38084e1c3487b392a133230ccea966466f15429
jim raynor <fallere82531@gmail.com>
2023-04-14 21:37:02 +0900

    Docs: Make common items of arrays non-duplicate I made a function for the assignment that receives three array values ​​and makes common items an array value without duplicates.

--- HaaardWork/Program.cs
+++ HaaardWork/Program.cs
@@ -4,27 +4,45 @@ namespace HaaardWork
 {
     internal class Program
     {
-        public static int SumOfDigits(int num)
+        public static int[] FindCommonItem(int[] arr1, int[] arr2, int[] arr3)
         {
-            int answer = 0;
-            while (true)
+            int[] arr0 = { };
+            int a = 0;
+            for (int i = 0; i < arr1.Length; i++)
             {
-                answer += num % 10;
-                num /= 10;
-                if (num <= 0)
+                for (int j = 0; j < arr2.Length; j++)
                 {
-                    break;
+                    for (int k = 0; k < arr3.Length; k++)
+                    {
+                        if (arr3[k] == arr2[j])
+                        {
+                            if (arr3[k] == arr1[i])
+                            {
+                                Array.Resize(ref arr0, a + 1);
+                                arr0[a] = arr3[k];
+                                a++;
+                            }
+                        }
+                    }
                 }
             }
+            int[] answer = arr0.Distinct().ToArray();
             return answer;
         }
 
+
         static void Main(string[] args)
         {
-            //주어진 숫자의 양의 정수의 각 자리수의 합을 구하는 solution을 완성하라
-            int a = 1521344589;
-            Console.WriteLine(SumOfDigits(a));
-            //42
+            //k개이 정렬된 배열에서 공통항목을 찾는 solution을 완성하라. 단 중복은 혀용하지 않는다.
+            int[] a = { 4,5,6,2,1,6,7,35,53,91,69};
+            int[] b = {35,7,7,9,90,123,35,45,74 };
+            int[] c = {35,7,45,123,123456789 };
+            int[] d = FindCommonItem(a, b, c);
+            foreach(int i in d)
+            {
+                Console.WriteLine(i);
+            }
+            //7, 35
         }
     }
 }
